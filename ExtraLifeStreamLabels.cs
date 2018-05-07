b6b3e3b5c3638965cb013fdbf4b365f0da83f234
Matt Kniller <wirejunky@live.com>
2018-05-07 08:23:20 -0400

    Added stream label file creation for participant data and most recent donation data

--- ExtraLifeStreamLabels.cs
+++ ExtraLifeStreamLabels.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Configuration;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.ServiceProcess;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using WireJunky.ExtraLife.DonorData;
@@ -13,6 +15,7 @@ using WireJunky.ExtraLife.ParticipantDataModel;
 
 // ReSharper disable UnusedVariable
 
+// ReSharper disable once CheckNamespace
 namespace WireJunky.ExtraLife.ExtraLifeStreamLabels
 {
     public partial class ExtraLifeStreamLabels : ServiceBase
@@ -35,13 +38,10 @@ namespace WireJunky.ExtraLife.ExtraLifeStreamLabels
 
             if (!EventLog.SourceExists(source))
                 EventLog.CreateEventSource(source, log);
-
-            //EventLog.WriteEntry(source, evt);
         }
 
         protected override void OnStart(string[] args)
         {
-            Debugger.Launch();
             BeginFetchingExtraLifeData();
         }
 
@@ -52,49 +52,51 @@ namespace WireJunky.ExtraLife.ExtraLifeStreamLabels
 
         private async Task BeginFetchingExtraLifeData()
         {
-            ParticipantData previousParticipantData = new ParticipantData();
-            DonorDataModel previousDonorData = new DonorDataModel();
+            if (!Directory.Exists($"{ConfigurationManager.AppSettings["StreamLabelOutputPath"]}"))
+                Directory.CreateDirectory($"{ConfigurationManager.AppSettings["StreamLabelOutputPath"]}");
+
             while (!_cts.IsCancellationRequested)
             {
-                //https://www.extra-life.org/api/participants/296936
-
                 using (var clientParticipant = new HttpClient())
                 {
                     clientParticipant.BaseAddress = new Uri(Url);
                     HttpResponseMessage responseParticipant = await clientParticipant.GetAsync(ParticipantEndpoint, _cts.Token);
                     if (responseParticipant.IsSuccessStatusCode)
                     {
-                        ParticipantData currentParticipantData =
-                            ParticipantData.FromJson(responseParticipant.Content.ReadAsStringAsync().Result);
-
-                        int percentage = Convert.ToInt32(currentParticipantData.GetPercentOfGoalReached());
-
-                        if (!previousParticipantData.Equals(currentParticipantData))
+                        using (FileStream progressDataStream = new FileStream($"{ConfigurationManager.AppSettings["StreamLabelOutputPath"]}//ExtraLifeProgress.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
                         {
-                            previousParticipantData = currentParticipantData;
-                            //TODO: Create and/or write out stream lables here. Do this to prevent writing the file if the data hasn't changed.
+                            ParticipantData currentParticipantData =
+                                ParticipantData.FromJson(responseParticipant.Content.ReadAsStringAsync().Result);
+
+                            int percentage = Convert.ToInt32(currentParticipantData.GetPercentOfGoalReached());
+
+                            string progress = $"{currentParticipantData.SumDonations} / {currentParticipantData.FundraisingGoal} ( {percentage}% )";
+                            progressDataStream.Write(new UTF8Encoding(true).GetBytes(progress), 0, progress.Length);
                         }
                     }
+                }
+                await Task.Delay(new TimeSpan(0, 0, 0, 15), _cts.Token);
 
-                    Debug.WriteLine($"Participant Data fetched at: {DateTime.Now.ToLongTimeString()}");
-                    await Task.Delay(new TimeSpan(0, 0, 0, 15), _cts.Token);
-                    
-                    using (var clientDonations = new HttpClient())
+                using (HttpClient clientDonations = new HttpClient())
+                {
+                    clientDonations.BaseAddress = new Uri(Url);
+                    HttpResponseMessage responseDonations = await clientDonations.GetAsync(_donationsEndpoint, _cts.Token);
+                    if (responseDonations.IsSuccessStatusCode)
                     {
-                        //DonorDataModel currentDonorData;
-                        clientDonations.BaseAddress = new Uri(Url);
-                        HttpResponseMessage responseDonations = await clientDonations.GetAsync(_donationsEndpoint, _cts.Token);
-                        if (responseDonations.IsSuccessStatusCode)
+                        using (FileStream lastDonorData = new FileStream($"{ConfigurationManager.AppSettings["StreamLabelOutputPath"]}//ExtraLifeMostRecentDonation.txt",
+                            FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
                         {
                             DonorDataModel[] donorList = DonorDataModel.FromJson(responseDonations.Content.ReadAsStringAsync().Result);
                             if (donorList.Any())
                             {
-                                DonorDataModel curreDataModel = donorList[0];
+                                DonorDataModel mostRecentDataModel = donorList[0];
+                                string donorName = mostRecentDataModel.DisplayName ?? "Anonymous";
+                                string donation = $"{donorName}: ${mostRecentDataModel.Amount}";
+                                lastDonorData.Write(new UTF8Encoding(true).GetBytes(donation), 0, donation.Length);
                             }
                         }
-                        Debug.WriteLine($"Fetched Donations at {DateTime.Now.ToLongTimeString()}");
-                        await Task.Delay(new TimeSpan(0, 0, 0, 15), _cts.Token);
                     }
+                    await Task.Delay(new TimeSpan(0, 0, 0, 15), _cts.Token);
                 }
             }
         }
